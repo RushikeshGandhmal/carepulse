0bb4ae04fce9356925c73ed4fb14f2f7476a4bda
Rushikesh Gandhmal <rushikeshgandhmal@gmail.com>
2024-07-13 22:35:58 +0530

    add Shadcn

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,10 @@
-import Image from "next/image";
+import { Button } from "@/components/ui/button";
 
 export default function Home() {
   return (
     <div>
-      <h1 className="text-3xl underline">
-        Hello
-      </h1>
+      <h1 className="text-white text-3xl underline">Hello</h1>
+      <Button>Click Me</Button>
     </div>
   );
 }
