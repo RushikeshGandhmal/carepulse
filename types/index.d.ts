ae4dc075f02691fd657c9eac0506b4f554cf57a3
RushikeshGandhmal <rushikeshgandhmal@gmail.com>
2024-07-21 20:57:46 +0530

    add SearchParamProps

--- types/index.d.ts
+++ types/index.d.ts
@@ -1,5 +1,10 @@
 /* eslint-disable no-unused-vars */
 
+declare type SearchParamProps = {
+  params: { [key: string]: string };
+  searchParams: { [key: string]: string | string[] | undefined };
+};
+
 declare type Gender = "Male" | "Female" | "Other";
 declare type Status = "pending" | "scheduled" | "cancelled";
 
