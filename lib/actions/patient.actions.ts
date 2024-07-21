c7da941c77fa7036295b5b8ad36c22a410976e26
RushikeshGandhmal <rushikeshgandhmal@gmail.com>
2024-07-21 20:58:10 +0530

    add getUser action

--- lib/actions/patient.actions.ts
+++ lib/actions/patient.actions.ts
@@ -22,3 +22,17 @@ export const createUser = async (user: CreateUserParams) => {
     }
   }
 };
+
+// GET USER
+export const getUser = async (userId: string) => {
+  try {
+    const user = await users.get(userId);
+
+    return parseStringify(user);
+  } catch (error) {
+    console.error(
+      "An error occurred while retrieving the user details:",
+      error
+    );
+  }
+};
